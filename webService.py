43fe4d809b96891f8528d8654bd4d006f3d83c2f
Robert Donnelly <g00358931@gmit.ie>
2021-01-07 17:48:24 +0000

    python and index communicating

--- webService.py
+++ webService.py
@@ -1,34 +1,21 @@
-import flask
-from tensorflow import  keras
+import flask 
+from tensorflow import keras
 import numpy as np
-import json
-from flask import request
+from flask import jsonify
 
-app = flask.flask(__name__)
+app = flask.Flask(__name__)
 
-model = keras.models.load_model("./model.h5")
 
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
 
-@app.route('/predict',methods =["POST"])
-def predict():
-    data = {"success":False}
-    print(model)
-    #get param
-    params = flask.request.form 
-    if(params == None):
-        params = flask.request.form 
-    if(params != None):
-        pValue = params['value']
-        myArray = np.array([pValue])
-        x = model.predict(myArray)
-        list = x.tolist()
-        jsonString = json.dumps(list)
-        data["response"] = jsonString
-        data["success"] = True
-    return flask.jsonify(data)#return in json
+@app.route("/predict/<input>")
+def predict(input):
+    model = keras.models.load_model('./model.h5')
+    input = float(input)
+    result = model.predict([input])
+    return jsonify({"value":result.item(0)})
 
 
 app.run(host="0.0.0.0")
